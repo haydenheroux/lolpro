17ca9137639b8d57ceb37a33b1af80fd7e98954d
Hayden Heroux <haydenheroux@gmail.com>
2024-10-03 14:09:51 -0400

    refactor(database): Make database private.

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -7,33 +7,33 @@ import (
 )
 
 type Database struct {
-	Database *gorm.DB
+	db *gorm.DB
 }
 
 func Create(dsn string) (*Database, error) {
-	database, err := gorm.Open(sqlite.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(sqlite.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		return &Database{}, err
 	}
 
 	// TODO Refactor to AutoMigrate([]interface{})
-	database.AutoMigrate(&model.Team{})
-	database.AutoMigrate(&model.Player{})
-	database.AutoMigrate(&model.PlayerMatchData{})
-	database.AutoMigrate(&model.Match{})
+	db.AutoMigrate(&model.Team{})
+	db.AutoMigrate(&model.Player{})
+	db.AutoMigrate(&model.PlayerMatchData{})
+	db.AutoMigrate(&model.Match{})
 
-	return &Database{Database: database}, nil
+	return &Database{db}, nil
 }
 
 func (d Database) SaveTeam(team *model.Team) error {
-	return d.Database.Save(team).Error
+	return d.db.Save(team).Error
 }
 
 func (d Database) GetTeams() ([]*model.Team, error) {
 	var teams []*model.Team
 
-	if err := d.Database.Find(&teams).Error; err != nil {
+	if err := d.db.Find(&teams).Error; err != nil {
 		return nil, err
 	}
 
@@ -43,7 +43,7 @@ func (d Database) GetTeams() ([]*model.Team, error) {
 func (d Database) GetTeam(id uint) (*model.Team, error) {
 	var team model.Team
 
-	if err := d.Database.Preload("Players").First(&team, "id = ?", id).Error; err != nil {
+	if err := d.db.Preload("Players").First(&team, "id = ?", id).Error; err != nil {
 		return nil, err
 	}
 
@@ -51,17 +51,17 @@ func (d Database) GetTeam(id uint) (*model.Team, error) {
 }
 
 func (d Database) SavePlayer(player *model.Player) error {
-	return d.Database.Save(player).Error
+	return d.db.Save(player).Error
 }
 
 func (d Database) SaveMatch(match *model.Match) error {
-	return d.Database.Save(match).Error
+	return d.db.Save(match).Error
 }
 
 func (d Database) GetMatches() ([]*model.Match, error) {
 	var matches []*model.Match
 
-	if err := d.Database.Find(&matches).Error; err != nil {
+	if err := d.db.Find(&matches).Error; err != nil {
 		return nil, err
 	}
 
@@ -69,5 +69,5 @@ func (d Database) GetMatches() ([]*model.Match, error) {
 }
 
 func (d Database) SaveMatchData(matchData *model.PlayerMatchData) error {
-	return d.Database.Save(matchData).Error
+	return d.db.Save(matchData).Error
 }
